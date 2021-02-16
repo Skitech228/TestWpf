79fabffc574291c8c24d0042723bddd4a742fbc7
Asus <Asus@DESKTOP-O3FTUTG>
2021-02-16 11:51:23 +0300

    Add Commands,StaticResource,InputBindings

---
+++ TestWpf/Infrostructure/Commands/CloseApplicationCommand.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using TestWpf.Infrostructure.Command.Base;
+
+namespace TestWpf.Infrostructure.Commands
+{
+    internal class CloseApplicationCommand : CommandBase
+    {
+        public override bool CanExecute(object parameter) => true;
+
+        public override void Execute(object parameter) => Application.Current.Shutdown();
+    }
+}
