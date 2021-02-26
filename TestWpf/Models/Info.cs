6868c3fcce4b886614c1e3289fc6c0e12f3b2420
Asus <Asus@DESKTOP-O3FTUTG>
2021-02-26 10:48:09 +0300

    ???

---
+++ TestWpf/Models/Info.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Media.Converters;
+
+namespace TestWpf.Models
+{
+    internal class PlaseInfo
+    {
+        public string name { get; set; }
+
+        public string location { get; set; }
+        public IEnumerable<ConfermdCount> ConfermdCounts { get; set; }
+    }
+
+    internal class ContrueInfo : PlaseInfo
+    {
+        public IEnumerable<ProvinseInfo> ProvinsecCounts { get; set; }
+    }
+
+    internal class ProvinseInfo : PlaseInfo{ }
+
+    internal struct ConfermdCount
+    {
+        public DateTime Date { get;set; }
+        public int Counts { get;set; }
+    }
+
+    internal struct DatePoint
+    {
+        public double YValues { get; set; }
+        public double XValues { get;set; }
+    }
+
+}
--- TestWpf/ViewModels/MainWindowViewModel.cs
+++ TestWpf/ViewModels/MainWindowViewModel.cs
@@ -1,8 +1,16 @@
 ﻿#region Using derectives
 
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Globalization;
+using System.Linq;
 using System.Windows;
 using System.Windows.Input;
+using OxyPlot;
 using TestWpf.Infrostructure.Commands;
+using TestWpf.Models;
+using DataPoint= OxyPlot.DataPoint;
 
 #endregion
 
@@ -12,7 +20,11 @@ namespace TestWpf.ViewModels.Base
     {
         private string _Title = "Анимэ";
 
-        public MainWindowViewModel() => CloseAppCommand = new LambdaCommand(OnCloseAppCommandExecute, CanCloseAppCommandExecute);
+        #region TestDataPoin:IList<DataPoint> - Текстовый набор данных для визуализации графиков
+
+        public IList<DataPoint> TestDataPoints { get; private set; }
+
+        #endregion
 
         /// <summary>
         ///     Заголовок окна
@@ -31,6 +43,47 @@ namespace TestWpf.ViewModels.Base
         private void OnCloseAppCommandExecute(object p)
         {
             Application.Current.Shutdown();
+        }
+
+        public IEnumerable<int> TicketGenerator()
+        {
+            var rnd = new Random();
+
+            while (true)
+                yield return rnd.Next(100000, 1000000);
+        }
+
+        private bool IsLucky(int number)
+        {
+            int firstHalf = int.Parse(number.ToString().Substring(0, 3));
+            int secondHalf = int.Parse(number.ToString().Substring(3, 3));
+
+            return firstHalf.ToString().Select(x => int.Parse(x.ToString())).Sum() ==
+                   secondHalf.ToString().Select(x => int.Parse(x.ToString())).Sum();
+        }
+        public MainWindowViewModel()
+        {
+            #region Commands
+
+            CloseAppCommand = new LambdaCommand(OnCloseAppCommandExecute, CanCloseAppCommandExecute);
+
+            #endregion
+
+            TestDataPoints = new List<DataPoint>();
+
+            //for (var x = 0d; x < 360; x+=0.1)
+            //{
+            //    const double to_rad = Math.PI / 180;
+            //    var y = Math.Sin(x * to_rad);
+            //    TestDataPoints.Add(new DataPoint (x,(double)y));
+            //}
+            for (int i = 1; i <= 10; i++)
+            {
+                var nums = TicketGenerator().Take(1000).ToArray();
+                int luckyTicketsCount = nums.Count(x => IsLucky(x));
+                TestDataPoints.Add(new DataPoint(i, (double)luckyTicketsCount / 1000 * 100));
+                Debug.WriteLine($"Всего билетов: {1000}\nСчастливых билетов: {luckyTicketsCount}\nПроцент счастливых билетов: {((double)luckyTicketsCount / 1000 * 100):0.###}%");
+            }
         }
     }
 }
